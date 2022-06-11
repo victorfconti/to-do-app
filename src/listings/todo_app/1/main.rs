45925ac0612e52fdd0a308cd9acda5f53e7d9a23
Victor Conti <victorfconti@gmail.com>
2022-06-11 01:03:16 -0300

    Initial release :

---
+++ src/listings/todo_app/1/main.rs
@@ -0,0 +1,27 @@
+mod window;
+
+use window::Window;
+use gtk::{Application};
+use gtk::prelude::*;
+
+fn main() {
+    // Initialize logger
+    pretty_env_logger::init();
+
+    // Create a new application
+    let app = Application::builder()
+        .application_id("org.gtk-rs.Todo")
+        .build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a new custom window and show it
+    let window = Window::new(app);
+    window.present();
+}
---
+++ src/listings/todo_app/1/window/mod.rs
@@ -0,0 +1,23 @@
+mod imp;
+
+// use crate::todo_object::TodoObject;
+// use crate::todo_row::TodoRow;
+use glib::{clone, Object};
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{gio, glib};
+use gtk::{Application, NoSelection, SignalListItemFactory};
+
+glib::wrapper! {
+    pub struct Window(ObjectSubclass<imp::Window>)
+        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
+
+impl Window {
+    pub fn new(app: &Application) -> Self {
+        // Create new window
+        Object::new(&[("application", app)]).expect("Failed to create `Window`.")
+    }
+}
